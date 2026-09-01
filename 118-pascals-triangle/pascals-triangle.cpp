e248ef3c367e76e34555058bc2f70f36b90de592
munjeshivika-ui <munjeshivika@gmail.com>
2026-09-01 22:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (62.56%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int row=1;row<=numRows;row++){
+            long long val=1;
+            vector<int> ansRow;
+            ansRow.push_back(1);
+            for(int col=1;col<row;col++){
+                val=val*(row-col);
+                val=val/col;
+                ansRow.push_back(val);
+            }
+            ans.push_back(ansRow);
+        }
+        return ans;
+        }
+    
+};
